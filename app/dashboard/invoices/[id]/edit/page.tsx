564a8cdb9116fed059f00bd294efaedc2ee77ab2
Wilfried Prombo <wilfriedprombo02@gmail.com>
2024-01-12 12:49:38 +0100

    :construction: Handling errors ✅

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,15 +1,20 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id
+  const id = params.id;
 
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
-    fetchCustomers()
-  ])
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+  
   return (
     <main>
       <Breadcrumbs
